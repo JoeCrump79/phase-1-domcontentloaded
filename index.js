34d2420c672abb2fec54ea72234e4a74686206a0
Joseph Crump <josephcrump79@yahoo.com>
2024-09-10 14:23:57 -0500

    phase-1-domcontentloaded

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    text.textContent = "This is really cool!";
+  });
+
+const text = document.getElementById("text")
+  
